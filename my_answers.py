c9d0c52a2336883e20031625433c863632896fe3
vilacham <matheuspires93@hotmail.com>
2018-06-05 10:14:18 -0300

    Backpropagation implementation
    Implements backpropagation function

--- my_answers.py
+++ my_answers.py
@@ -56,36 +56,37 @@ class NeuralNetwork(object):
         
         return final_outputs, hidden_outputs
 
-    def backpropagation(self, final_outputs, hidden_outputs, X, y, delta_weights_i_h, delta_weights_h_o):
-        ''' Implement backpropagation
-         
+    def backpropagation(self, final_output, hidden_output, X, y, delta_w_i_h,
+                        delta_w_h_o):
+        """ Implement backpropagation
+
             Arguments
             ---------
-            final_outputs: output from forward pass
+            final_output: output from forward pass
             y: target (i.e. label) batch
-            delta_weights_i_h: change in weights from input to hidden layers
-            delta_weights_h_o: change in weights from hidden to output layers
+            delta_w_i_h: change in weights from input to hidden layers
+            delta_w_h_o: change in weights from hidden to output layers
+        """
+        # Output error (difference between desired target and actual output)
+        error = y - final_output
 
-        '''
-        #### Implement the backward pass here ####
-        ### Backward pass ###
+        # Hidden layer's contribution to the error (product between weights of
+        # the hidden layer and output error)
+        hidden_error = np.dot(self.w_hidden_output, error)
 
-        # TODO: Output error - Replace this value with your calculations.
-        error = None # Output layer error is the difference between desired target and actual output.
-        
-        # TODO: Calculate the hidden layer's contribution to the error
-        hidden_error = None
-        
-        # TODO: Backpropagated error terms - Replace these values with your calculations.
-        output_error_term = None
+        # Backpropagated error terms (product between the respective error and
+        # the derivative of the respective activation function). For the output
+        # layer, f'(x) = 1. For the hidden layer, f'(x) = f(x) * (1 - f(x)).
+        output_error_term = error
+        hidden_error_term = hidden_error * hidden_output * (1 - hidden_output)
 
-        hidden_error_term = None
-        
-        # Weight step (input to hidden)
-        delta_weights_i_h += None
-        # Weight step (hidden to output)
-        delta_weights_h_o += None
-        return delta_weights_i_h, delta_weights_h_o
+        # Weight step (input to hidden: product between hidden error term and
+        # input values)
+        delta_w_i_h += hidden_error_term * X[:, None]
+        # Weight step (hidden to output: product between output error term and
+        # hidden layer activation values)
+        delta_w_h_o += output_error_term * hidden_output[:,  None]
+        return delta_w_i_h, delta_w_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
         ''' Update weights on gradient descent step
