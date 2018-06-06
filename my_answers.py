7677f1b402371e9dcf48ed47821b02118e950b9e
vilacham <matheuspires93@hotmail.com>
2018-06-05 23:56:08 -0300

    Set hyperparameters
    Set hyperparameters after training (a lot) and execute the last cells of
    the notebook.

--- my_answers.py
+++ my_answers.py
@@ -126,7 +126,7 @@ class NeuralNetwork(object):
 
 
 # Set hyperparameters below
-iterations = 100
-learning_rate = 0.1
-hidden_nodes = 2
+iterations = 10000
+learning_rate = 0.3
+hidden_nodes = 13
 output_nodes = 1
